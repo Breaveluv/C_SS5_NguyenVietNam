aa8bcef73c587213d08058643b36773ac7cdb09b
Breaveluv <nam.bule12@gmail.com>
2024-11-26 01:30:55 +0700

    Add files via upload

---
+++ SS5_Ex5.cpp
@@ -0,0 +1,18 @@
+#include(stdio.h)
+  int main () {
+    int  num a=1 , numb ;
+    
+	   printf("bang cuu chuong cua so %d\n", num a);
+    for(int i =1;i<=11;i++){
+        num b =num a*i;
+        printf("%d x %d = %d \n",numa ,i,num b);
+        if( i==10){
+            if(num a ==9){
+                break;
+            }
+            i=0;
+            num++;
+            printf("bang cuu chuong cua so %d\n", num a) ; 
+            }
+    return 0;
+	} 
